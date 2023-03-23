e94caeffd85a29ef274b66addf76170c081977a0
KrozhDev <roko.mdn@gmail.com>
2023-03-23 19:01:38 +0300

    Troubles with loadFromFile method

--- src/com/company/Basket.java
+++ src/com/company/Basket.java
@@ -1,30 +1,46 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Collection;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Queue;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static java.util.Arrays.stream;
 
 public class Basket {
 
-    private int[] prices;
+    private long[] prices;
     private String[] goods;
-    private int amount;
-    private int productNum;
-    private HashMap <Integer,Integer> cart;
+    private long amount;
+    private long productNum;
+    private HashMap<Long, Long> cart;
 
-    public  Basket(String[] goods, int[] prices) {
+
+
+    public Basket() {
+        goods = new String[]{"Хлеб", "Яблоки", "Молоко"};
+        prices = new long[]{100, 200, 300};
+        this.cart = new HashMap<>();
+    }
+    public Basket(String[] goods, long[] prices) {
         this.goods = goods;
         this.prices = prices;
         this.cart = new HashMap<>();
 
     }
 
-    public void addToCart(int productNum, int amount) {
+    public String[] getGoods() {
+        return this.goods;
+    }
+
+    public long[] getPrices() {
+        return this.prices;
+    }
+
+    public void addToCart(long productNum, long amount) {
         this.cart.put(productNum, amount);
     }
 
@@ -34,33 +50,90 @@ public class Basket {
 
         String stringForFile = this.cart.keySet().stream().
                 map(key -> key + " " + this.cart.get(key)).
-                collect(Collectors.joining(",","{","}"));
+                collect(Collectors.joining(" "));
 
         System.out.println(stringForFile);
     }
 
-    public void saveTxt(File textFile){
+    public void saveTxt(File textFile) {
         System.out.println("Сохраняю корзину в файл");
-
-
         try (FileOutputStream fos = new FileOutputStream(textFile.getPath())) {
 // перевод строки в массив байтов
-
-            //todo надо вытащить из мапы ключи и значения для дальнейшей записи в файл
-
             String stringForFile = this.cart.keySet().stream().
                     map(key -> key + " " + this.cart.get(key)).
-                    collect(Collectors.joining(",","{","}"));
+                    collect(Collectors.joining(" "));
 
             byte[] bytes = stringForFile.getBytes();
 // запись байтов в файл
             fos.write(bytes, 0, bytes.length);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
+        }
     }
-//todo доделать выгрузку из файла
-//    public static Basket loadFromTxtFile(File textFile) {
-//        System.out.println("Выгружаю из фала");
+
+// выгрузка из файла
+    public static Basket loadFromTxtFile(File textFile) {
+        System.out.println("Выгружаю из фала");
+        FileInputStream fos = null;
+        Basket basket = null;
+        try {
+            fos = new FileInputStream(textFile.getPath());
+            int i;
+            StringBuilder fileContents = new StringBuilder();
+            while ((i = fos.read()) != -1) {
+                if (i == 32) continue;
+                fileContents.append(i);
+            }
+            basket = new Basket();
+            long prodNum = 0;
+            long prodAm = 0;
+            long counter = 0;
+            for (int idx = 0; idx < fileContents.length(); idx++) {
+                if (idx % 2 == 0) {
+                    prodNum = fileContents.charAt(idx);
+                } else {
+                    prodAm = fileContents.charAt(idx);
+                }
+                counter++;
+                if (counter > 0 & counter % 2 == 0) {
+                    basket.addToCart(Character.getNumericValue((char) prodNum), Character.getNumericValue((char) prodAm));
+                }
+            }
+
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return basket;
+    }
+
+//todo геттеры и сеттеры?
+
+
+//     public String toString() {
+//
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (String elem: this.goods) {
+//            sb.append(elem + " ");
+//        }
+//        sb.append("\n");
+//        for (long elem: this.prices) {
+//            sb.append(elem + " ");
+//        }
+//
+//        return sb.toString();
 //    }
 
-}}
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -17,22 +18,84 @@ public class Main {
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+//
+//        System.out.println("Hell");
+//
+//        String[] goods = {"Хлеб", "Яблоки", "Молоко"};
+//        long[] prices = {100, 200, 300};
+//
+//        Basket basket = new Basket(goods,prices);
+//        basket.addToCart(1,1);
+//        basket.addToCart(2,2);
+//        basket.addToCart(3,3);
+//
+//        basket.printCart();
+//        basket.saveTxt(textFile);
+//        Basket basket2 = Basket.loadFromTxtFile(textFile);
 
-        System.out.println("Hell");
 
-        String[] products = {"Хлеб", "Яблоки", "Молоко"};
-        int[] prices = {100, 200, 300};
+        Basket basket = Basket.loadFromTxtFile(textFile);
+        int productNumber = 0;
+        int productCount = 0;
+//        int currentPrice = prices[productNumber];
 
-        Basket basket = new Basket(products,prices);
-        basket.addToCart(1,1);
-        basket.addToCart(2,2);
-        basket.addToCart(3,3);
-
-        basket.printCart();
-        basket.saveTxt(textFile);
+        for (int i = 0; i < basket.getGoods().length; i++) {
+            System.out.println(i+1 + " " + basket.getGoods()[i] + " " + basket.getPrices()[i] + " руб/шт");
+        }
 
 
 
+        while (true) {
+
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Выберите товар и количество или введите `end`");
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                basket.printCart();
+                //todo перебрать корзину и вывести Хлеб 3 шт 100 руб/шт 300 руб в сумме
+                //todo Яблоки 1 шт 200 руб/шт 200 руб в сумме
+                System.out.println("Всего рублей...");
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            if (parts.length > 2 | parts.length == 1) {
+                System.out.println("Введите два значения через пробел и нажмите Enter!");
+                System.out.println();
+                continue;
+            }
+
+            try {
+                productNumber = Integer.parseInt(parts[0]);
+                if (productNumber <= 0 | productNumber > basket.getGoods().length) {
+                    System.out.println();
+                    System.out.println("Введите нормальный номер товара");
+                    System.out.println();
+                    continue;
+
+                }
+                productCount = Integer.parseInt(parts[1]);
+                if (productCount <= 0 | productCount > 100) {
+                    System.out.println();
+                    System.out.println("Введите нормальное количесвто товара!");
+                    System.out.println();
+                    continue;
+                }
+                basket.addToCart(productNumber, productCount);
+                basket.saveTxt(textFile);
+//                numOfChosenGoods[productNumber - 1] = productCount;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Вы ввели не число!");
+            } finally {
+                continue;
+            }
+
+
+
+        }
 //        String[] products = {"Хлеб", "Яблоки", "Молоко"};
 //        int[] prices = {100, 200, 300};
 //        int[] numOfChosenGoods = new int[3];
